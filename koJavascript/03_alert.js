149ec947084a205b1e3c8c6d67e0fb7c3ddb2474
DuhanMo <devduhanmo@gmail.com>
2021-01-18 18:42:15 +0900

    2021/01/18 study

---
+++ koJavascript/03_alert.js
@@ -0,0 +1,40 @@
+// let age = prompt('나이를 입력해주세요.', 100);
+
+// alert(`당신의 나이는 ${age}살 입니다.`);
+
+// let isBoss = confirm("당신이 주인인가요?")
+// alert(isBoss);
+
+// let a = 1, b = 1;
+
+// let c = ++a; // ?
+// let d = b++; // ?
+
+console.log("" + 1 + 0) //10 문자형
+console.log("" - 1 + 0)//-10 -> -1
+console.log(true + false)//true -> 1
+console.log(6 / "3")//2
+console.log("2" * "3")//6
+console.log(4 + 5 + "px")//9px
+console.log("$" + 4 + 5)//$45
+console.log("4" - 2)//2
+console.log("4px" - 2)//4px-2 -> NaN
+console.log(7 / 0)//Infinity
+console.log("  -9  " + 5)// -95 -> -9 5
+console.log("  -9  " - 5)// -14 ->
+console.log(null + 1)//null -> 1
+console.log(undefined + 1)//undefined -> NaN
+console.log(" \t \n" - 2)//-2
+
+// let a = prompt("덧셈할 첫 번째 숫자를 입력해주세요.", 1);
+// let b = prompt("덧셈할 두 번째 숫자를 입력해주세요.", 2);
+
+// alert(+a + +b); 
+
+let a = +prompt("덧셈할 첫 번째 숫자를 입력해주세요.", 1);
+let b = +prompt("덧셈할 두 번째 숫자를 입력해주세요.", 2);
+
+alert(a + b); // 12
+
+
+
