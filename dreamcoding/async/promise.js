f4521a755fb54f785ac5d4d76b057a17ef09e95d
DuhanMo <devduhanmo@gmail.com>
2021-01-26 16:46:10 +0900

    2021/01/26 callback, promise, async, await

--- dreamcoding/async/promise.js
+++ dreamcoding/async/promise.js
@@ -9,7 +9,7 @@
 // when new Promise is created, the executor runs automatically
 // 최종데이터를 전달하는 resolve, 문제가 생기면  reject 콜백함수 생성 
 // 네트워크나 파일을 불러오는 시간이 오래걸리는 일들은 프로미스를 써서 함.
-const promise = new Promise((resolve, reject)=>{
+const promise = new Promise((resolve, reject) => {
     // doing some heavy work(network, read files)
     console.log('doing something..');
     setTimeout(() => {
@@ -21,15 +21,15 @@ const promise = new Promise((resolve, reject)=>{
 // 2. Counsumers: then, catch, finally
 // promise 내부 resolve 를통해 전달된 값이 value를 통해 들어옴 
 promise //
-.then((value) => {
-    console.log(value);
-})
-.catch((error) => {
-    console.log(error);
-})
-.finally(() => {
-    console.log('finally');
-});
+    .then((value) => {
+        console.log(value);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+    .finally(() => {
+        console.log('finally');
+    });
 
 // 3. Promise chaining
 const fetchNumber = new Promise((resolve, reject) => {
@@ -37,11 +37,33 @@ const fetchNumber = new Promise((resolve, reject) => {
 });
 
 fetchNumber
- .then(num => num *2)
- .then(num => num *3)
- .then(num => {
-     return new Promise((resolve, reject) => {
-         setTimeout(() => resolve(num - 1), 1000);
-     });
- })
- .then(num => console.log(num));
+    .then(num => num * 2)
+    .then(num => num * 3)
+    .then(num => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => resolve(num - 1), 1000);
+        });
+    })
+    .then(num => console.log(num));
+
+const getHen = () => new Promise((resolve, reject) => {
+    setTimeout(() => resolve('닭'), 1000);
+});
+const getEgg = hen => new Promise((resolve, reject) => {
+    // setTimeout(() => resolve(`${hen} => 달걀`), 1000);
+    setTimeout(() => reject(new Error(`error !! ${hen} => 달걀`)), 1000);
+});
+const cook = egg => new Promise((resolve, reject) => {
+    setTimeout(() => resolve(`${egg} => 계란후라이`), 1000);
+});
+// getHen()
+// .then(hen => getEgg(hen))
+// .then(egg => cook(egg))
+// .then(meal => console.log(meal));
+
+getHen() //
+    .then(getEgg)
+    .catch(error => '새로운거')
+    .then(cook)
+    .then(console.log)
+    .catch(console.log);
